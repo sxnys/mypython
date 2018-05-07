009846e17aa7de43108fae4cce6e48ff38d3fd18
unknown <1119112647@qq.com>
2018-05-07 22:10:20 +0800

    da ti ka 2

--- opencv/答题卡/test.py
+++ opencv/答题卡/test.py
@@ -1,17 +1,32 @@
+"""
+根据文本进行图像倾斜矫正
+"""
+import numpy as np
 import cv2
 
-# 加载图片，将它转换为灰阶，轻度模糊，然后边缘检测。
-image = cv2.imread('testimg.jpg')
-# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-# edged = cv2.Canny(blurred, 75, 200)
+# 加载图片，将它转换为灰阶
+img = cv2.imread('initial.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.bitwise_not(gray)
 
-# cv2.imwrite('testimg1.jpg', edged)
+# 二值化，将图片黑白色反转
+thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
-ret, thresh = cv2.threshold(image, 127, 255, 0)
-img, contours, *rest = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-color = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-image = cv2.drawContours(color, contours, -1, (0, 255, 0), 2)
-cv2.imshow('contours', color)
-cv2.waitKey()
-cv2.destroyAllWindows()
+# 得到旋转角度
+coords = np.column_stack(np.where(thresh > 0))
+angle = cv2.minAreaRect(coords)[-1]
+angle = -(90 + angle) if angle < -45 else -angle
+
+print(angle)
+
+# 执行仿射变换对倾斜角度校正
+h, w = img.shape[:2]
+center = (w // 2, h // 2)
+M = cv2.getRotationMatrix2D(center, angle, 1.0)
+rotated = cv2.warpAffine(thresh, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+
+# cv2.imshow('rotated', rotated)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+cv2.imwrite('rotated.jpg', rotated)
+
